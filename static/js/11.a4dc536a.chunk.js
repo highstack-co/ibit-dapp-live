02ca4c234914f629f249bb389073dc9aa8f57ffd
highstack-co <hello@highstack.co>
2023-05-29 21:56:49 +0900

    Updates

---
+++ static/js/11.a4dc536a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpstaking-frontend"]=this["webpackJsonpstaking-frontend"]||[]).push([[11],{1024:function(n,s){}}]);
+//# sourceMappingURL=11.a4dc536a.chunk.js.map
---
+++ static/js/12.211b8a4a.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpstaking-frontend"]=this["webpackJsonpstaking-frontend"]||[]).push([[12],{1059:function(n,s){}}]);
+//# sourceMappingURL=12.211b8a4a.chunk.js.map
